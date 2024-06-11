823fab5de7ecd0ea20367dc00ae17b0517bb3ff0
S-kkipie <admamaniq@unsa.edu.pe>
2024-06-11 12:38:31 -0500

    last ver

--- api/views.py
+++ api/views.py
@@ -25,6 +25,7 @@ def plate_create(request):
     serializer = PlateSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
+        return Response(serializer.data)
     return Response(serializer.data)
 
 
